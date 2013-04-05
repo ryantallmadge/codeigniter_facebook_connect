17ffab8cbb67f5e8db8843208dd0e85653d49b25
Ryan Tallmadge <ryantallmadge@gmail.com>
2013-04-05 10:36:31 -0400

    updated code comments

--- application/controllers/facebook.php
+++ application/controllers/facebook.php
@@ -1,26 +1,66 @@
-<?php
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+Copyright (C) 2011 by Ryan Tallmadge
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+
+	/**
+	 * Class Facebook - Used to control the interaction with the facebook API
+	 */
+
 	class Facebook extends CI_Controller {
 
         public $facebook_session;//Will hold the facebook session vars
         public $facebook_loginURL; //Will hold the login URL for redirects
         public $facebook_user; //Will hold the facebook user data till the session takes over
 
+		/**
+		 * Start the Facebook class
+		 * Include the library's and sessions needed to run the methods in the class
+		 */	
 		function __construct()
 		{
+			//Load the parent class data
 			parent::__construct();
             //Load the facebook session - Needed for the two way communicate that happens
             $this->facebook_session  = $this->facebook_connect->getSession();
-            $this->load->model('user_model');
+            //Load the user model to store and retrive data from the users database
+			$this->load->model('user_model');
+			//Load the Facebook API library to communicate with facebook
             $this->load->library('facebook_connect');
 		}
 
-        //If someone comes to the index redirect them
+		/**
+		 * Redirects user if they reach the index by mistake
+		 *
+		 */ 
 		function index()
 		{
             redirect(base_url());
 		}
 
-        //Some has asked to be loged in, lets do it
+		/**
+		 * User has asked to by logged in, lets process the request
+		 */
 		function login()
 		{
             //check and see if we have a facebook session
@@ -53,7 +93,9 @@
             }
 		}
 
-        //Log the user out of facebook session and gstify session
+		/**
+		 * User has asked to by logged out, kill all sessions
+		 */
 		function logout()
 		{
             if(!$this->facebook_session){
--- application/models/user_model.php
+++ application/models/user_model.php
@@ -1,15 +1,50 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+Copyright (C) 2011 by Ryan Tallmadge
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+/**
+ * Defines the users interaction with the database.
+ */
 
 class user_model extends CI_Model {
 
+	//Hold any data to be returned to the controler
     public $return_data;
 
+	//Start the class by including the parent class
     function __construct()
 	{
 		parent::__construct();
 	}
-
-    //Log a user in
+	
+	
+	/**
+	 * Logs the users into the website by starting a session
+	 *
+	 * @param array $session_data all parameters that are needed to be logged into the website
+	 * @return bool
+	 */
+ 
     public function LogInUser($session_data){
         //Let see if we can add the user
         if(!$this->AddUser($session_data)){
@@ -21,12 +56,19 @@ class user_model extends CI_Model {
         return true;
     }
 
+	/**
+	 * Create a user account into the database
+	 *
+	 * @param array $insert_array list of vars to be put into the database
+	 * @return bool
+	 */	
     public function AddUser($insert_array){
         //Lets check and see if we already have the user in the system
         $run_check_query = $this->db->query("SELECT * FROM users WHERE user_id = ".$this->db->escape($insert_array['user_id']).";");
         if($run_check_query->num_rows() > 0) return true;
         //We dont have the user so lets add them to the system
         $insert_user = $this->db->insert_string('users',$insert_array);
+		//If there is an error log it
         if(!$this->db->query($insert_user)){
             log_message(
                         'error',
